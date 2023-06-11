83897f938fcfe20b08784183b34210cacd5a827b
Kriggerino <tmhkma18@gmail.com>
2023-06-11 16:24:01 +0700

    Editable grid size added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('button');
-let sizeOfGrid = 24;
+let firstGrid = 24;
 const createGrid = (amountOfGrid) =>{
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
     for (let i = 0; i < amountOfGrid; i++){
         //Create row div
         const row = document.createElement('div')
@@ -9,7 +11,7 @@ const createGrid = (amountOfGrid) =>{
         
         for (let j = 0; j < amountOfGrid; j++){
             //Inside row, create and append gridbox div to row div
-            const widthAndHeight = 800/ sizeOfGrid
+            const widthAndHeight = 800/ amountOfGrid
             const gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
             gridBox.style.width = `${widthAndHeight}px`
@@ -21,8 +23,26 @@ const createGrid = (amountOfGrid) =>{
             row.appendChild(gridBox)
         }
         //Add row to container
-        container.appendChild(row)
+        wrapper.appendChild(row)
     }
+    container.appendChild(wrapper)
 }
 
-createGrid(sizeOfGrid)
+createGrid(firstGrid) // Init default grid 
+
+resetButton.addEventListener('click', () =>{
+    let userSize = Number(prompt('New grid size?'))
+    while(userSize > 100){
+         let userSize = Number(prompt('New grid size? 100 is too big'))
+
+    }
+    const wrapper = document.querySelector('.wrapper')
+    if(!wrapper){
+        createGrid(userSize)
+    } else {
+        wrapper.remove()
+        createGrid(userSize)
+    }
+    
+})
+
